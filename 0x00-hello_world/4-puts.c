836793a481f593802cb6b1874f261db0d1ebd29c
diegorose172 <diegorosero172@gmail.com>
2021-10-05 21:54:56 -0700

    again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main(void)
 /**
- *main prints the phrase Programing is like a ...
+ *main prints Programming is like ...
  *
  */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
