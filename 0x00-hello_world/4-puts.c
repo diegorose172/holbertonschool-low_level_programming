764a12ab633b6abcbf3d59164e31c9a8d4898016
diegorose172 <diegorosero172@gmail.com>
2021-10-05 22:15:57 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 /**
-* main - this supposed to have a description here
+* int main (void) - this supposed to have a description here
 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
