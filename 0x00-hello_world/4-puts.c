4b2e6ac35aa9c80e9ee92dad99e9cdccd0adb429
diegorose172 <diegorosero172@gmail.com>
2021-10-05 22:02:17 -0700

    fixed code betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
-/**
- *is there suppose to be a description here bra
- *
- */
 #include <stdio.h>
+
 /**
- *main prints Programming is like ...
- *
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-/**
- *maybe here
- */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
